440775d2684760885956c99b4f75ca609451aa95
eaejaum <jpedroalmeida179@gmail.com>
2025-01-20 10:49:49 -0300

    styles from login page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,101 @@
-import { StyleSheet, Text, View } from "react-native";
+import colors from "@/constants/colors";
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Login() {
-    return (
-        <View style={styles.container}>
-            <Text>Login</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.logoText}>
+          Dev<Text style={{ color: colors.green }}>App</Text>
+        </Text>
+        <Text style={styles.slogan}>O futuro da programação</Text>
+      </View>
+      <View style={styles.form}>
+        <View>
+          <Text style={styles.label}>Email</Text>
+          <TextInput placeholder="Digite seu email..." style={styles.input} />
         </View>
-    )
+
+        <View>
+          <Text style={styles.label}>Senha</Text>
+          <TextInput
+            placeholder="Digite sua senha..."
+            secureTextEntry
+            style={styles.input}
+          />
+        </View>
+
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </Pressable>
+
+        <Link href="/(auth)/signup/page" style={styles.link}>
+            <Text>Ainda não possui uma conta? Cadastre-se</Text>
+        </Link>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    } 
+  container: {
+    flex: 1,
+    paddingTop: 80,
+    backgroundColor: colors.zinc,
+  },
+  header: {
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  logoText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.white,
+    marginBottom: 8,
+  },
+  slogan: {
+    fontSize: 37,
+    color: colors.white,
+    marginBottom: 34,
+  },
+  form: {
+    flex: 1,
+    backgroundColor: colors.white,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    paddingTop: 24,
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  label: {
+    color: colors.zinc,
+    marginBottom: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: colors.gray,
+    paddingHorizontal: 8,
+    paddingTop: 14,
+    paddingBottom: 14,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: colors.green,
+    paddingTop: 14,
+    paddingBottom: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  buttonText: {
+    color: colors.white,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 16,
+    textAlign: "center",
+  }
 });
