207746c60e9a5d23ed3b2321c96f314a14a70073
eaejaum <jpedroalmeida179@gmail.com>
2025-01-20 10:57:23 -0300

    styles from signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,17 +1,116 @@
-import { StyleSheet, Text, View } from "react-native";
+import colors from "@/constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Signup() {
-    return (
-        <View style={styles.container}>
-            <Text>Cadastro</Text>
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+
+        <Pressable style={styles.backButton} onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={24} color={colors.white}/> 
+        </Pressable>
+
+        <Text style={styles.logoText}>
+          Dev<Text style={{ color: colors.green }}>App</Text>
+        </Text>
+        <Text style={styles.slogan}>Criar uma conta</Text>
+      </View>
+      <View style={styles.form}>
+        <View>
+          <Text style={styles.label}>Nome Completo</Text>
+          <TextInput placeholder="Digite seu nome..." style={styles.input} />
         </View>
-    )
+
+        <View>
+          <Text style={styles.label}>Email</Text>
+          <TextInput placeholder="Digite seu email..." style={styles.input} />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Senha</Text>
+          <TextInput
+            placeholder="Digite sua senha..."
+            secureTextEntry
+            style={styles.input}
+          />
+        </View>
+
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    } 
+  container: {
+    flex: 1,
+    paddingTop: 80,
+    backgroundColor: colors.zinc,
+  },
+  header: {
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  logoText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.white,
+    marginBottom: 8,
+  },
+  slogan: {
+    fontSize: 37,
+    color: colors.white,
+    marginBottom: 34,
+  },
+  form: {
+    flex: 1,
+    backgroundColor: colors.white,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    paddingTop: 24,
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  label: {
+    color: colors.zinc,
+    marginBottom: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: colors.gray,
+    paddingHorizontal: 8,
+    paddingTop: 14,
+    paddingBottom: 14,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: colors.green,
+    paddingTop: 14,
+    paddingBottom: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  buttonText: {
+    color: colors.white,
+    fontWeight: "bold",
+  },
+  backButton: {
+    backgroundColor: "rgba(255, 255, 255, 0.55)",
+    alignSelf: "flex-start",
+    padding: 8,
+    borderRadius: 8,
+    marginBottom: 8,
+  }
 });
